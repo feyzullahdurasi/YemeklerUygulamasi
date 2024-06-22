7c2f6c6d81827aee799a55310f456cb39356851b
Feyzullah Durası <durasifeyzullah672@gmail.com>
2024-06-22 09:27:17 +0300

    Initial Commit

---
+++ YemeklerUygulamasi/YemeklerUygulamasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  YemeklerUygulamasiApp.swift
+//  YemeklerUygulamasi
+//
+//  Created by Feyzullah Durası on 22.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct YemeklerUygulamasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
